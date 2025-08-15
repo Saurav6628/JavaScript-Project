6659feec6242683c4fb3dbb56b1825bbf0b7e377
Saurav Gupta <99986187+Saurav6628@users.noreply.github.com>
2025-08-15 16:11:18 +0000

    Project 2 modified and Project 3 Added

--- project2/backend.js
+++ project2/backend.js
@@ -4,30 +4,34 @@ button.addEventListener('click',(event)=>{
     let weight=document.querySelector("#weight").value;
     height=Number(height);
     weight=Number(weight);
-    let result=(weight*(10000/height));
-    let res=document.querySelector("#result");
-    res.textContent=`${result.toFixed(2)}`;
-    let ans1,ans2,ans3;
-    ans1=document.querySelector("#stat1");
-    ans2=document.querySelector("#stat2");
-    ans3=document.querySelector("#stat3");
-    if (result<18.6)
-    {
-        
-        ans1.style.backgroundColor="green";
-        ans2.style.backgroundColor="white";
-        ans3.style.backgroundColor="white";
-    }
-    else if (result>=18.6 && result<24.9)
-    {
-        ans1.style.backgroundColor="white";
-        ans2.style.backgroundColor="green";
-        ans3.style.backgroundColor="white";
-    }
+    if(height==0 || weight==0) alert("Enter a valid height or weight!!!")
     else
     {
-        ans1.style.backgroundColor="white";
-        ans2.style.backgroundColor="white";
-        ans3.style.backgroundColor="green";
+        let result=(weight*(10000/height));
+        let res=document.querySelector("#result");
+        res.textContent=`${result.toFixed(2)}`;
+        let ans1,ans2,ans3;
+        ans1=document.querySelector("#stat1");
+        ans2=document.querySelector("#stat2");
+        ans3=document.querySelector("#stat3");
+        if (result<18.6)
+        {
+            
+            ans1.style.backgroundColor="green";
+            ans2.style.backgroundColor="white";
+            ans3.style.backgroundColor="white";
+        }
+        else if (result>=18.6 && result<24.9)
+        {
+            ans1.style.backgroundColor="white";
+            ans2.style.backgroundColor="green";
+            ans3.style.backgroundColor="white";
+        }
+        else
+        {
+            ans1.style.backgroundColor="white";
+            ans2.style.backgroundColor="white";
+            ans3.style.backgroundColor="green";
+        }
     }
 })
